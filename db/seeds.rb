5f484dd82a3b5575d1781c1b3286510db87ccdfb
shimizu1023 <214320444+shimizu1023@users.noreply.github.com>
2025-10-13 20:03:14 +0900

    カテゴリデータ

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,5 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+categories = %w[大阪府 京都府 兵庫県 奈良県 和歌山県 滋賀県 三重県]
+
+categories.each do |name|
+  Category.find_or_create_by!(name: name)
+end
